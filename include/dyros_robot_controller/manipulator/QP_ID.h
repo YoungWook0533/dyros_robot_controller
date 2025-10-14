3a7f6a951764bbf10eb000f1e4810027b899ec85
YoungWook0533 <monglee147@gmail.com>
2025-10-14 11:02:35 +0900

    add comments to C++

--- include/dyros_robot_controller/manipulator/QP_ID.h
+++ include/dyros_robot_controller/manipulator/QP_ID.h
@@ -7,15 +7,41 @@ namespace drc
 {
     namespace Manipulator
     {
+        /**
+         * @brief Class for solving inverse dynamics QP problems for manipulators.
+         * 
+         * This class inherits from QPBase and implements methods to set up and solve
+         * inverse dynamics problems for manipulators using Quadratic Programming.
+         */
         class QPID : public QP::QPBase
         {
             public:
                 EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+                /**
+                 * @brief Constructor.
+                 * @param robot_data (std::shared_ptr<Manipulator::RobotData>)
+                 *                   Shared pointer to the RobotData class.
+                 */
                 QPID(std::shared_ptr<Manipulator::RobotData> robot_data);
+                /**
+                 * @brief Set the desired task space acceleration for the link.
+                 * @param xddot_desired (Eigen::VectorXd) Desired task space acceleration.
+                 * @param link_name     (std::string) Name of the link.
+                 */
                 void setDesiredTaskAcc(const VectorXd &xddot_desired, const std::string &link_name);
+                /**
+                 * @brief Get the optimal joint acceleration and torque by solving QP.
+                 * @param opt_qddot   (Eigen::VectorXd) Optimal joint accelerations.
+                 * @param opt_torque  (Eigen::VectorXd) Optimal joint torques.
+                 * @param time_status (TimeDuration) Output time durations structure for the QP solving process.
+                 * @return (bool) True if the problem was solved successfully.
+                 */
                 bool getOptJoint(VectorXd &opt_qddot, VectorXd &opt_torque, QP::TimeDuration &time_status);
 
             private:
+                /**
+                 * @brief Struct to hold the indicies of the QP variables and constraints.
+                 */
                 struct QPIndex
                 {
                     // decision variables
@@ -60,15 +86,39 @@ namespace drc
 
                 }si_index_;
 
-                std::shared_ptr<Manipulator::RobotData> robot_data_;
+                std::shared_ptr<Manipulator::RobotData> robot_data_;   // Shared pointer to the robot data class.
                 
-                int joint_dof_;
-                VectorXd xddot_desired_;
-                std::string link_name_;
+                int joint_dof_;             // Number of joints in the manipulator
+                VectorXd xddot_desired_;    // Desired task space acceleration
+                std::string link_name_;     // Name of the link
                 
+                /**
+                 * @brief Set the cost function which minimizes task space acceleration error.
+                 *        Use slack variables to increase feasibility of QP.
+                 *
+                 *         min       || x_ddot_des - J*qddot - Jdot * qdot ||_2^2
+                 * [qddot, torque]
+                 *
+                 * =>      min        1/2 * [ qddot  ]^T * [2 * J.T * J  0] * [ qddot  ] + [-2 * J.T * (x_ddot_des - Jdot * qdot)].T * [ qddot  ]
+                 * [qddot, torque]          [ torque ]     [     0       0]   [ torque ]   [                 0                   ]     [ torque ]
+                 */
                 void setCost() override;
+                /**
+                 * @brief Set the bound constraint to keep all slack variables non-negative.
+                 */
                 void setBoundConstraint() override;
+                /**
+                 * @brief Set the inequality constraints which limit manipulator joint angles and velocities, avoid singularity and self collision.
+                 */
                 void setIneqConstraint() override;
+                /**
+                 * @brief Set the equality constraint which forces joint accelerations and torques to match the equations of motion.
+                 *
+                 * subject to M * qddot + g = torque
+                 *
+                 * => subject to [M -I][ qddot  ] = [-g]
+                 *                     [ torque ]
+                 */
                 void setEqConstraint() override;
         };
     } // namespace QP
